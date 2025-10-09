c469c897241cc0fa1a240e6b0f8fe4652de561a2
valeriacadenay <valeria.cadena@unsimon.edu.co>
2025-10-09 11:28:32 -0500

    feat: addin Service veterinary and the repository

---
+++ petmanagment/Repositories/ServiceVeterinaryRepository.cs
@@ -0,0 +1,37 @@
+﻿using petmanagment.Interfaces;
+using petmanagment.Models;
+using petmanagment.Data;
+
+namespace petmanagment.Repositories;
+
+public class ServiceVeterinaryRepository : IServiceVeterinary 
+{
+    public ServiceVeterinaryRepository()
+    {
+    }
+    
+    public List<ServiceVeterinary> GetAll()
+    {
+        return DataBase.services;
+    }
+    
+    public ServiceVeterinary? GetById(Guid id)
+    {
+        return DataBase.services.Find(service => service.Id == id);
+    }
+
+    public void CreateService(ServiceVeterinary service)
+    {
+        DataBase.services.Add(service);
+    }
+
+    public void UpdateService(ServiceVeterinary service, string id)
+    {
+        DataBase.services = DataBase.services.Select((serv => serv.Id.ToString() == id ? service : serv)).ToList();
+    }
+
+    public void Delete(Guid id)
+    {
+        DataBase.services = DataBase.services.Where((serv => serv.Id == id)).ToList();
+    }
+}
---
+++ petmanagment/interfaces/IServiceVeterinary.cs
@@ -0,0 +1,13 @@
+﻿using petmanagment.Models;
+
+namespace petmanagment.Interfaces;
+
+public interface IServiceVeterinary
+{
+    List<ServiceVeterinary> GetAll();
+    ServiceVeterinary? GetById(Guid id);
+    void CreateService(ServiceVeterinary service);
+    void UpdateService(ServiceVeterinary service, string id);
+    void Delete(Guid id);
+    
+}
