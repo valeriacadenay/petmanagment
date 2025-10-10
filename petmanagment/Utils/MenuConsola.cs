8718b0631404ab8095ad63517356f643eeaf108a
valeriacadenay <valeria.cadena@unisimon.edu.co>
2025-10-09 19:57:12 -0500

    feat changes on the menu

--- petmanagment/Utils/MenuConsola.cs
+++ petmanagment/Utils/MenuConsola.cs
@@ -1,3 +1,5 @@
+using petmanagment.Services;
+
 namespace petmanagment.Models.Interface;
 
 public class MenuConsola
@@ -8,23 +10,86 @@ public class MenuConsola
 
         Console.WriteLine("");
         Console.WriteLine("------------ MAIN MENU ------------");
-        Console.WriteLine("1. Register patient");
-        Console.WriteLine("2. List patients");
-        Console.WriteLine("3. Search patient by ID");
-        Console.WriteLine("4. Search patients by Age or Species");
-        Console.WriteLine("5. Request a veterinary service");
+        Console.WriteLine("1. Register a new Client");
+        Console.WriteLine("2. List Clients");
+        Console.WriteLine("3. Search Clients by Name");
+        Console.WriteLine("4. Update Client Information");
+        Console.WriteLine("5. Delete Client");
+        Console.WriteLine("6. Register a new Patient");
+        Console.WriteLine("7. List Patients");
+        Console.WriteLine("8. Update Patient Information");
+        Console.WriteLine("10. Search Patients by ID");
+        Console.WriteLine("11. Delete Patient");
+        Console.WriteLine("12. Register a new Veterinary");
+        Console.WriteLine("13. List Veterinaries");
+        Console.WriteLine("14. Search Veterinaries by ID");
+        Console.WriteLine("15. Update Veterinary Information");
+        Console.WriteLine("16. Delete Veterinary");
+        Console.WriteLine("17. Request a veterinary service");
         Console.WriteLine("0. Exit");
         Console.Write("Select an option: ");
-    }
+        
+        string option = Console.ReadLine();
+        ConsoleInterface.ShowSeparator();
 
-    public static void VeterinaryServiceMenu()
-    {
-        ConsoleInterface.ShowTitle("Veterinary Service Menu");
-        Console.WriteLine("");
-        Console.WriteLine("1. General Consultation");
-        Console.WriteLine("2- Vaccination");
-        Console.WriteLine("3. Back to the main menu");
-        Console.Write("Select an option: ");
-       
+        switch (option)
+        {
+            case "0":
+                Console.WriteLine("Exiting the program. Goodbye!");
+                Environment.Exit(0);
+                break;
+            case "1":
+                OwnerService.CreateOwner();
+                break;
+            case "2":
+                OwnerService.GetOwners();
+                break;
+            case "3":
+                OwnerService.GetOwnerByName();
+                break;
+            case "4":
+                OwnerService.
+                break;
+            case "5":
+                ClientService.DeleteClient();
+                break;
+            case "6":
+                PatientService.RegisterPatient();
+                break;
+            case "7":
+                PatientService.ListPatients();
+                break;
+            case "8":
+                PatientService.UpdatePatientInformation();
+                break;
+            case "10":
+                PatientService.SearchPatientById();
+                break;
+            case "11":
+                PatientService.DeletePatient();
+                break;
+            case "12":
+                VeterinaryService.RegisterVeterinary();
+                break;
+            case "13":
+                VeterinaryService.ListVeterinaries();
+                break;
+            case "14":
+                VeterinaryService.SearchVeterinaryById();
+                break;
+            case "15":
+                VeterinaryService.UpdateVeterinaryInformation();
+                break;
+            case "16":
+                VeterinaryService.DeleteVeterinary();
+                break;
+            case "17":
+                ServiceVeterinaryService.RequestVeterinaryService();
+                break;
+            default:
+                Console.WriteLine("Invalid option. Please try again.");
+                break;
+        }
     }
+    
 }
